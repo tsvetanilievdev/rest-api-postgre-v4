92799241a051f297ecae45d43ba0ff81518bc592
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2023-02-27 18:12:59 +0200

    add router and middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
+import router from './router';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log('Hello from express');
-    res.status(200);
-    res.json({ message: 'hello' });
-})
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/api', router);
+
 
 export default app;
